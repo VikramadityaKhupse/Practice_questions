d7dfced17f10bc64e642a6e36bd75057e2b26770
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-03-29 12:24:32 +0530

    Mid sem question practice

--- test.java
+++ test.java
@@ -1,75 +1,18 @@
-class Exam {
+class Question {
 
-static {
+    public static void main(String[] args) {
+        String str1 = "Mid";
+        String str2 = new String("Term");
+        String str3 = "Mid";
+        String str4 = str2;
+        String str5 = new String("Term");
 
-System.out.println("Static Block 1: EXAM"); 
-
-}
-
-Exam() {
-
-System.out.println("EXAM Constructor");
-
-
-
-}
-
-{
-
-System.out.println("Init Block: EXAM"); 
-
-}
-
-static {
-
-System.out.println("Static Block 2: EXAM"); 
-
-}
-
-}
-
-class MidTerm extends Exam {
-
-static {
-
-System.out.println("Static Block 1: MIDTERM"); 
-
-}
-
-MidTerm() {
-
-System.out.println("MIDTERM Constructor"); 
-
-}
-
-public String toString() {
-
-return "MidTerm";
-
-}
-
-static {
-
-System.out.println("Static Block 2: MIDTERM");
-
-}
-
-public static void main(String args[]) {
-
-System.out.println("Welcome To Mid Term Examiniation"); 
-
-Exam exam = new Exam();
-
-MidTerm javaProgramming = new MidTerm();
-
-System.out.println(javaProgramming); 
-
-}
-
-{
-
-System.out.println("Init Block: MIDTERM"); 
-
-}
-
+        System.out.println(str1.getClass().getName() + Integer.toHexString(str1.hashCode()) );
+        System.out.println(str2.getClass().getName() + Integer.toHexString(str2.hashCode()) );
+        System.out.println(str3.getClass().getName() + Integer.toHexString(str3.hashCode()) );
+        System.out.println(str4.getClass().getName() + Integer.toHexString(str4.hashCode()) );
+        System.out.println(str5.getClass().getName() + Integer.toHexString(str5.hashCode()) );
+    
+    
+    }
 }
