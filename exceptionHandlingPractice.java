e16801906a0d772639aa2f7eabdbaa6f6521a259
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-04-03 22:42:01 +0530

    Excetion Handling practice

--- exceptionHandlingPractice.java
+++ exceptionHandlingPractice.java
@@ -6,17 +6,17 @@ public static void main(String [] args){
 	try{
 	System.out.println(args[0]);
 	}
-	catch(Exception e){
-		Throwable t = new Throwable("This is a throwable", e);
-		//System.out.println(t.initCause(e));
-		System.out.println("\n"+t.getCause());
-		System.out.println("\n"+t.getStackTrace());
-		System.out.println("\n"+t.getSuppressed());
-		//e.printStackTrace();
-		
-		System.out.println("Exception in thread \"main\" " + t.getClass()+":"+  t.getMessage() + "\n\tat Practice.main");
-		
+	catch(Throwable e){
+		System.out.println("Inside catch");
 	
+	
+	}catch(Exception e){
+		System.out.println("Inside Throwable catch");
+	
+	
+	}finally{
+	
+	System.out.println("Inside Finally");
 	
 	}
 
