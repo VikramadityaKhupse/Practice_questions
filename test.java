5ed5bb12c6a3a6c7a050a73c513b3918fb0726ee
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-04-20 22:14:16 +0530

    ISE-II question practice

--- test.java
+++ test.java
@@ -6,12 +6,19 @@ class Question {
         String str3 = "Mid";
         String str4 = str2;
         String str5 = new String("Term");
-
+        try{
+        Thread.sleep(1213);
         System.out.println(str1.getClass().getName() + Integer.toHexString(str1.hashCode()) );
+        Thread.sleep(1213);
         System.out.println(str2.getClass().getName() + Integer.toHexString(str2.hashCode()) );
+        Thread.sleep(1213);
         System.out.println(str3.getClass().getName() + Integer.toHexString(str3.hashCode()) );
         System.out.println(str4.getClass().getName() + Integer.toHexString(str4.hashCode()) );
-        System.out.println(str5.getClass().getName() + Integer.toHexString(str5.hashCode()) );
+        System.out.println(str5.getClass().getName() + Integer.toHexString(str5.hashCode()) );}
+        catch(InterruptedException e){
+
+        
+        }
     
     
     }
