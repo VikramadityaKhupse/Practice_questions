61dda42919e9a4539f68a6325b34cea4172d6e54
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-05-20 11:25:11 +0530

    Created function for updating files and folder count and printing folder/files in given directory path

--- GetFilesList.java
+++ GetFilesList.java
@@ -1,26 +1,39 @@
 import java.io.File;
 
 public class GetFilesList {
+
+    static int filesCount;
+    static int foldersCount;
     public static void main(String[] args) {
         // Specify the directory path
         String directoryPath = "path/to/your/directory";
+        getFilesFolder(directoryPath);
+        System.out.println(filesCount);
+        System.out.println(foldersCount);
 
-        // Create a File object
-        File directory = new File(directoryPath);
+    }
 
-        // Get all the files and directories
+    public static void getFilesFolder(String path){
+
+        File directory = new File(path);
         File[] filesList = directory.listFiles();
 
-        if (filesList != null) {
-            for (File file : filesList) {
-                if (file.isFile()) {
-                    System.out.println("File: " + file.getName());
-                } else if (file.isDirectory()) {
-                    System.out.println("Directory: " + file.getName());
+        if(filesList != null){
+
+            for (File file : filesList){
+                if(file.isFile()){
+                    filesCount++;
+                    System.out.println("File" + file.getName());
+                }else if(file.isDirectory()){
+                    foldersCount++;
+                    System.out.println("Folder" + file.getName());
                 }
+
+
             }
-        } else {
-            System.out.println("The specified path is not a directory or an error occurred.");
+        } else{
+
+            System.out.println("Maybe path you provided is wrong! Provide correct path.");
         }
     }
 }
