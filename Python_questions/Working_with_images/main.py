b6b68df17b348751282df7b599ac2aa08da0f8af
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-06-22 17:45:49 +0530

    Added comments

--- Python_questions/Working_with_images/main.py
+++ Python_questions/Working_with_images/main.py
@@ -3,13 +3,21 @@
 
 from PIL import Image
 
+
 table_image = Image.open('word_matrix.png')
 mask = Image.open('mask.png')
 
 print(table_image.size)
 print(mask.size)
 
+# resizing mask image to the size of table image
 resized_mask_image = mask.resize(table_image.size)
+
+# making mask image transparent by changing alpha value of RGBA
 resized_mask_image.putalpha(100)
+
+# pasting image one over other
 table_image.paste(resized_mask_image, box=(0, 0), mask=resized_mask_image)
+
+# saving new image
 table_image.save('decoded_image.png')
