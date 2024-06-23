e306dc874f5b7698c9fdfb15cd710e271b5e8ae5
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-06-23 16:57:02 +0530

    Advanced string methods in python

---
+++ Python_questions/Advanced_objects_and_ds/advanced_strings.py
@@ -0,0 +1,19 @@
+string1 = "I am Vikramaditya"
+print(string1.capitalize())
+print(string1.upper())
+print(string1.lower())
+print(string1.count('a'))
+print(string1.find('a')) # first occurrence of a
+print(string1.center(30, '|'))
+print(string1.isalnum())
+print(string1.isalpha())
+print(string1.isdigit())
+print(string1.islower())
+print('Hello\tthere'.expandtabs(tabsize=20))
+print(string1.lstrip('I am'))
+print(string1.splitlines())
+print(string1.split(' '))
+print(string1.partition('Vik'))
+
+
+
